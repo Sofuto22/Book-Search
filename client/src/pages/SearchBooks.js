ed67ac65f7958f34aa526015d4d5ace72c19094c
Sofuto22 <zenigata19@gmail.com>
2023-02-09 14:39:05 -0500

    added mutation to search book

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -4,6 +4,8 @@ import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'reac
 import Auth from '../utils/auth';
 import { saveBook, searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
+import {useMutation} from "@apollo/client"
+import {SAVE_BOOK} from "../utils/mutation";
 
 const SearchBooks = () => {
   // create state for holding returned google api data
@@ -14,6 +16,8 @@ const SearchBooks = () => {
   // create state to hold saved bookId values
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
 
+  
+  
   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
   // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
   useEffect(() => {
@@ -52,10 +56,12 @@ const SearchBooks = () => {
     }
   };
 
+  const [saveBook] = useMutation(SAVE_BOOK)
+  
   // create function to handle saving a book to our database
-  const handleSaveBook = async (bookId) => {
+  //const handleSaveBook = async (bookId) => {
     // find the book in `searchedBooks` state by the matching id
-    const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
+    //const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
 
     // get token
     const token = Auth.loggedIn() ? Auth.getToken() : null;
@@ -140,6 +146,6 @@ const SearchBooks = () => {
       </Container>
     </>
   );
-};
+
 
 export default SearchBooks;
