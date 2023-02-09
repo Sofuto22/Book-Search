1246aca78b0153711d1b0084ba304784bc87d69c
Sofuto22 <zenigata19@gmail.com>
2023-02-09 13:17:55 -0500

    tried moving to my repo

---
+++ Book-Finder/server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const path = require('path');
+const db = require('./server/config/connection');
+const routes = require('./server/routes');
+const { authMiddleware } = require("./server/utils/auth");
+const { ApolloServer } = require("apollo-server-express");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const startApolloServer = async(typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({app});
+}
+
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.use(routes);
+
+db.once('open', () => {
+  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+});
+
+
+startApolloServer(typeDefs, resolvers);
